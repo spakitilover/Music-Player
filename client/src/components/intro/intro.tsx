f5c1794bb9fba93cbfc4fd330e4b76b2781e9a8b
unknown <herogame1970@gmail.com>
2023-04-13 18:58:32 +0000

    adding changes

--- client/src/components/intro/intro.tsx
+++ client/src/components/intro/intro.tsx
@@ -5,81 +5,102 @@ const Intro = () => {
     <div className="p-5 w-full ml-[300px] mt-[100px]">
       <div className="text-3xl font-['poppins'] text-white p-5">Albums</div>
       <div className="flex flex-wrap gap-10 justify-center p-5">
-        <div className="w-[200px] h-[200px] overflow-hidden  ">
-          <img
-            className=" object-cover rounded-lg "
-            src="https://static-cse.canva.com/blob/1035322/1600w-1Nr6gsUndKw.jpg"
-          />
+        <div>
+          <div className="w-[200px] h-[200px] overflow-hidden rounded-md  ">
+            <img
+              className="object-cover cursor-pointer hover:opacity-80 hover:scale-105 duration-300"
+              src="https://images.genius.com/3840f9abae5f6d441628f2e4c8f54e04.517x517x1.jpg"
+            />
+          </div>
+          <div className="text-white p-3 font-[poppins]">PAUSE FLOW</div>
         </div>
-        <div className="w-[200px] h-[200px] overflow-hidden  ">
-          {" "}
-          <img
-            className=" object-cover rounded-lg "
-            src="https://static.stereogum.com/uploads/2021/12/Best-Jazz-Albums-2021-1638893270.jpg"
-          />
+        <div>
+          <div className="w-[200px] h-[200px] overflow-hidden rounded-md  ">
+            {" "}
+            <img
+              className=" object-cover cursor-pointer  hover:opacity-80 hover:scale-105 duration-300"
+              src="https://media.pitchfork.com/photos/5e273d36d7f8cd0008150f58/1:1/w_600/eminem_music.jpg"
+            />
+          </div>
+          <div className="text-white p-3 font-[poppins]">Eminem</div>
         </div>
-        <div className="w-[200px] h-[200px] overflow-hidden ">
-          {" "}
-          <img
-            className=" object-cover rounded-lg"
-            src="https://media.npr.org/assets/img/2019/12/05/tyler-the-creator-igor_custom-967b87cf7029273e8d6975883b44a31a1d43a26e-s1100-c50.jpg"
-          />
+        <div>
+          <div className="w-[200px] h-[200px] overflow-hidden rounded-md ">
+            {" "}
+            <img
+              className=" object-cover cursor-pointer  hover:opacity-80 hover:scale-105 duration-300"
+              src="https://i.scdn.co/image/ab6761610000e5eb6d9d36888cf60c07345622a3"
+            />
+          </div>
+          <div className="text-white p-3 font-[poppins]">T-FLOW</div>
         </div>
-        <div className="w-[200px] h-[200px]  overflow-hidden ">
-          {" "}
-          <img
-            className=" object-cover rounded-lg "
-            src="https://i.scdn.co/image/ab67616d0000b273dc139ce5434df86c492a93df"
-          />
+        <div>
+          <div className="w-[200px] h-[200px]  overflow-hidden rounded-md ">
+            {" "}
+            <img
+              className=" object-cover cursor-pointer  hover:opacity-80 hover:scale-105 duration-300 "
+              src="https://yt3.googleusercontent.com/ytc/AGIKgqPfEfAWZaGDcCZjDo55mLDlb-Tqj3M_YOrIJdIqgQ=s900-c-k-c0x00ffffff-no-rj"
+            />
+          </div>
+          <div className="text-white p-3 font-[poppins]">System of a down</div>
         </div>
-        <div className="w-[200px] h-[200px] overflow-hidden ">
-          {" "}
-          <img
-            className=" object-cover rounded-lg "
-            src="https://media.npr.org/assets/img/2019/12/05/tyler-the-creator-igor_custom-967b87cf7029273e8d6975883b44a31a1d43a26e-s1100-c50.jpg"
-          />
+        <div>
+          <div className="w-[200px] h-[200px] overflow-hidden rounded-md ">
+            {" "}
+            <img
+              className=" object-cover cursor-pointer  hover:opacity-80 hover:scale-105 duration-300 "
+              src="https://resources.tidal.com/images/758bda3f/0ac8/407e/b3d4/d6037ac067cc/640x640.jpg"
+            />
+          </div>
+          <div className="text-white p-3 font-[poppins]">Nightcore</div>
         </div>
-        <div className="w-[200px] h-[200px] rounded-md  overflow-hidden ">
-          {" "}
-          <img
-            className=" object-cover"
-            src="https://i.scdn.co/image/ab67616d0000b273dc139ce5434df86c492a93df"
-          />
+        <div>
+          <div className="w-[200px] h-[200px] rounded-md overflow-hidden">
+            {" "}
+            <img
+              className=" object-cover cursor-pointer  hover:opacity-80 hover:scale-105 duration-300"
+              src="https://res.klook.com/image/upload/x_519,y_0,w_881,h_1234,c_crop/c_fill,ar_1:1/v1659946710/events_admin/kgvluv2e4qe5nzptjtcz.jpg"
+            />
+          </div>
+          <div className="text-white p-3 font-[poppins]">Alan Wolker</div>
         </div>
       </div>
-      <div className="w-[50vw] h-[350px]">
+      <hr className="mb-2 border-rose-600" />
+      <div className="w-[50vw] h-[350px] ">
         <ul className="flex w-full mb-3">
           <li className="text-white flex-1">
-            <h2 className=" flex justify-center text-slate-500">Songs</h2>
+            <h2 className=" flex  text-slate-500 text-sm">Songs</h2>
           </li>
           <li className="text-white flex-1 ">
             {" "}
-            <h2 className=" text-slate-500">Albums</h2>
+            <h2 className=" text-slate-500 font-[poppins] text-sm">Albums</h2>
           </li>
           <li className="text-white flex-2">
             {" "}
-            <h2 className=" text-slate-500">Time</h2>
+            <h2 className=" text-slate-500 font-[poppins] text-sm">Time</h2>
           </li>
         </ul>
-        <ul className="flex w-full mb-3 hover:bg-rose-500 duration-300 cursor-pointer p-2 rounded-lg">
-          <li className="text-white flex-1 flex items-center gap-2">
+        <ul className="flex w-full mb-3 hover:bg-rose-500 duration-300 cursor-pointer rounded-lg">
+          <li className="text-white flex-1 flex items-center gap-2 ml-3">
             <div className="w-[40px] h-[40px] rounded-md overflow-hidden">
               <img
                 className=" object-cover"
                 src="https://i.scdn.co/image/ab67616d0000b273dc139ce5434df86c492a93df"
               />
             </div>
-            <span className="font-[poppins]">7LIWA - SEÑORITA FT. DJ </span>
+            <span className="font-[poppins] text-sm w-[50%]">
+              7LIWA - SEÑORITA FT. DJ{" "}
+            </span>
           </li>
           <li className="text-white flex-1 ">
             <div className="h-[50px] flex items-center">
-              <div className="font-[poppins]">7liwa</div>
+              <div className="font-[poppins] text-sm">7liwa</div>
             </div>
           </li>
           <li className="text-white flex-2">
             {" "}
             <div className="h-[50px] flex items-center">
-              <div className="font-[poppins]">3:28</div>
+              <div className="font-[poppins] text-sm mr-1">3:28</div>
             </div>
           </li>
         </ul>
--- client/src/components/playlist/playlist.tsx
+++ client/src/components/playlist/playlist.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import StarBorder from "@mui/icons-material/StarBorder";
 import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
+import PlayArrow from "@mui/icons-material/PlayArrow";
+import Pause from "@mui/icons-material/Pause";
+import SkipNext from "@mui/icons-material/SkipNext";
+import SkipPrevious from "@mui/icons-material/SkipPrevious";
+import Slider from "@mui/material/Slider";
+import Stack from "@mui/material/Stack";
+import VolumeDown from "@mui/icons-material/VolumeDown";
+import VolumeUp from "@mui/icons-material/VolumeUp";
 
 const Playlist = () => {
   return (
-    <div className="border-t-[1px] bg-black flex items-center border-rose-500 h-[150px] w-full fixed bottom-0 z-40">
+    <div className="border-t-[1px] bg-black flex items-center border-rose-500 h-[100px] w-full fixed bottom-0 z-40">
       <div className="text-white flex items-center justify-between w-full p-10">
-        <div className="flex">
-          <div className="w-[100px] h-[100px] rounded-md overflow-hidden">
+        <div className="flex justify-start items-center  w-[30%]">
+          <div className="w-[70px] h-[70px] rounded-md overflow-hidden">
             <img
               className=" object-cover"
               src="https://i.scdn.co/image/ab67616d0000b273dc139ce5434df86c492a93df"
@@ -23,10 +31,33 @@ const Playlist = () => {
           </div>
         </div>
 
-        <div>
-          <audio />
+        <div className="w-[40%] flex justify-center">
+          <div>
+            <div className="flex justify-center gap-5">
+              <div className=" cursor-pointer hover:bg-rose-500 p-1  rounded-full duration-200">
+                <SkipPrevious style={{ fontSize: "40px" }} />
+              </div>
+              <div className=" cursor-pointer hover:bg-rose-500 p-1  rounded-full duration-200">
+                <PlayArrow style={{ fontSize: "40px" }} />
+              </div>
+              <div className=" cursor-pointer hover:bg-rose-500 p-1  rounded-full duration-200">
+                <SkipNext style={{ fontSize: "40px" }} />
+              </div>
+            </div>
+            <div className="w-[30vw] flex items-center gap-5">
+              <div className="text-sm font-[poppins] text-slate-500">0:00</div>
+              <Slider
+                size="small"
+                defaultValue={70}
+                aria-label="Small"
+                valueLabelDisplay="auto"
+              />
+              <div className="text-sm font-[poppins] text-slate-500">3:32</div>
+            </div>
+          </div>
         </div>
-        <div className="flex gap-3">
+
+        <div className=" gap-3 w-[30%] flex justify-end">
           <div>
             <FavoriteBorder />
           </div>
